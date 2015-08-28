8c5a30d8349083472e362fb47d6f020a0537379e
Mircea Tanasa <tanasamircea@yahoo.com>
2015-08-28 12:59:09 +0300

    *Added files for projects archive page and for single project page

---
+++ single-projects.php
@@ -0,0 +1,14 @@
+<?php get_header() ?>
+
+<div class="single-project">
+  <div class="midpage">
+    <div class="page_title">
+      <h1>Title</h1>
+      <p>DEsc</p>
+    </div>
+    
+
+  </div>
+</div>
+
+<?php get_footer() ?>
