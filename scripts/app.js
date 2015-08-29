fd467b661224ce6a68650cb488b649077909894b
Mircea Tanasă <tanasamircea@yahoo.com>
2015-08-29 16:21:38 +0300

    *Portfolio script updated

--- scripts/app.js
+++ scripts/app.js
@@ -7,13 +7,9 @@
     this.displayer      = new Displayer($('.displayer', this.el))
 
     this.el.on('filter', this.onFilter.bind(this));
-    this.el.on('project-zoom', this.onZoomProj.bind(this));
   }
   App.prototype.onFilter = function(event, selected) {
     this.projectListing.filter(selected);
-  }
-  App.prototype.onZoomProj = function(event) {
-    this.displayer.zoomProj(event);
   }
   $(function(){
     var app = new App($('.portofolio'),projects);
